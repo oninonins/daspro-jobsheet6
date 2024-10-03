12bc4e92f8ffc765d0a19afede582841fcc9d275
oninonins <varelinobubadibako11@gmail.com>
2024-10-03 12:44:21 +0700

    new

---
+++ Pemilihan2Percobaan105.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class Pemilihan2Percobaan105 {
+    public static void main(String[] args) {
+      Scanner input05 = new Scanner(System.in);
+
+        
+        int tahun ;
+        System.out.print("Masukan Tahun : ");
+        tahun = input05.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) !=0) {
+                System.out.println("Tahun kabisat");
+            }
+        }else 
+            System.out.println("Bukan Tahun Kabisat");
+
+
+    }
+    
+}
