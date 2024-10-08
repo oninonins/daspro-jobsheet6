7c3ce078875694b96338b17c2835a390f2fbdb6e
oninonins <varelinobubadibako11@gmail.com>
2024-10-08 15:21:41 +0700

    tanpa op logika

--- Pemilihan2Percobaan105.java
+++ Pemilihan2Percobaan105.java
@@ -1,33 +1,26 @@
 import java.util.Scanner;
 
-
 public class Pemilihan2Percobaan105 {
     public static void main(String[] args) {
-      Scanner input05 = new Scanner(System.in);
+        Scanner input05 = new Scanner(System.in);
 
-        
-        int tahun ;
-        System.out.print("Masukan Tahun : ");
+        int tahun;
+        System.out.print("Masukkan Tahun: ");
         tahun = input05.nextInt();
 
-       
-        if ((tahun % 4) == 0) {
-            if ((tahun % 100) !=0 && tahun % 400 == 0) {
-                System.out.println(tahun + "Tahun kabisat" );
-            }else {
-                System.out.println(tahun + "Bukan tahun kabisat");
+        
+        if (tahun % 4 == 0) {
+            if (tahun % 100 == 0) {
+                if (tahun % 400 == 0) {
+                    System.out.println(tahun + " adalah tahun kabisat.");
+                } else {
+                    System.out.println(tahun + " bukan tahun kabisat.");
+                }
+            } else {
+                System.out.println(tahun + " adalah tahun kabisat.");
             }
+        } else {
+            System.out.println(tahun + " bukan tahun kabisat.");
         }
-        else{
-            System.out.println(tahun + "bukan tahun kabisat");
-        }
-        
-            
-            
-            
-        
-
-
     }
-    
 }
